83d0df3dfa109f87fa6bdf2c05216bdbe5c54b77
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-10 23:08:24 +0700

    Define the Validations

--- routes/blockchain.route.js
+++ routes/blockchain.route.js
@@ -5,4 +5,21 @@ router.get('/', function(req, res) {
     res.render('blockchain');
 })
 
+router.post('/post', function(req, res) {
+    let name = req.body.name;
+    let bandwidth = req.body.bandwidth;
+
+    req.checkBody('name', 'Name is Required').notEmpty();
+    req.checkBody('bandwidth', 'Bandwidth is required').notEmpty();
+
+    var errors = req.validationErrors();
+    if(errors){
+        req.session.errors = errors;
+        req.session.success = false;
+        res.redirect('blockchain');
+    } else {
+        req.session.success = true;
+        res.redirect('/');
+    }
+})
 module.exports = router;
