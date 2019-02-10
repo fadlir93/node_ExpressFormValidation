8c1295fddfcf746696ffcc78779bdb6019b5da64
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-10 23:00:08 +0700

    Define Express Router

--- server.js
+++ server.js
@@ -7,15 +7,17 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session')
+const blockchain = require('./routes/blockchain.route');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(expressValidator());
 app.use(cookieParser());
 app.use(session({secret: 'krunal', saveUninitialized: false, resave: false}));
+app.use('/blockchain', blockchain)
 
 app.engine('hbs', hbs.express4 ({
-    partialsDir: __dirname + '/views/partials'
+    partialsDir: __dirname + '/views'
 }));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views')
