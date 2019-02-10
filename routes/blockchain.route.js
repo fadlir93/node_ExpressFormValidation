510c760ad2df9de316fa1a78100156a611400806
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-10 23:17:55 +0700

    Display the errors

--- routes/blockchain.route.js
+++ routes/blockchain.route.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('blockchain');
+    res.render('blockchain', { success: req.session.success, errors: req.session.errors});
+    req.session.errors = null;
 })
+
 
 router.post('/post', function(req, res) {
     let name = req.body.name;
@@ -16,7 +18,7 @@ router.post('/post', function(req, res) {
     if(errors){
         req.session.errors = errors;
         req.session.success = false;
-        res.redirect('blockchain');
+        res.redirect('/blockchain');
     } else {
         req.session.success = true;
         res.redirect('/');
