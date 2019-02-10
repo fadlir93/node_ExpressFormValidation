1b21c8eb611d35bd6931e6a1ea926ab46f37c672
Fadli Ramadhan <fadlir93@gmail.com>
2019-02-10 22:29:51 +0700

    Install and Config Handlebars

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const expressValidator = require('express-validator');
 const PORT = 3000;
+const hbs = require('express-hbs');
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.engine('hbs', hbs.express4 ({
+    partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views')
+
 
 app.listen(PORT,function(req, res) {
     console.log('Server is running on PORT: ', PORT)
